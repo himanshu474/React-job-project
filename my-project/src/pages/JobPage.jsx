01d71ce0a72a7409336f26ba07477d6800f78b51
himanshu474 <himanshurawat474@gmail.com>
2024-03-26 03:14:07 +0530

    new commit

--- my-project/src/pages/JobPage.jsx
+++ my-project/src/pages/JobPage.jsx
@@ -33,7 +33,7 @@ const JobPage = ({ deleteJob }) => {
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
             <main>
               <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
-                <div className="text-gray-400 mb-4 ">{job.type}</div>
+                <div className="text-gray-400 mb-4 "> {job.type}</div>
                 <h1 className="text-3xl font-bold mb-4">{job.title}</h1>
                 <div className="text-gray-400 mb-4 flex align-middle justify-center md:justify-start">
                   <FaMapMarker className="text-orange-600 mr-1" />
